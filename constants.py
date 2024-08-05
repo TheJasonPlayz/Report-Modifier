71710818b23ddf9b261e044b0108af34b30f1bb1
Jason Whitman <thejasonplayz@gmail.com>
2024-08-05 14:05:15 -0600

    Remove PageNums and add DOCX converter constants

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ XLSX = f"data.xlsx"
 TEMPLATE = f"{OUTPUT}/template.html"
 METRICS = f"{OUTPUT}/metrics.txt"
 PARSER = "html5lib"
-
-class PageNums(Enum):
-    Single = auto()
-    Multiple = auto()
+DOCX_FILES = [f for f in listdir() if f.endswith(".docx")]
+ID_TO_AREA = dict([search(r"([0-9|a-zA-Z]+)?\s?([0-9|a-zA-Z]+)?", f).groups(2) for f in listdir(f"{OUTPUT}/") if f.endswith(".docx") or f.endswith(".pdf")])
+# IDS = [search(r"([0-9]+?)?\s?(?:[a-zA-Z]+?)?", f)[1] for f in listdir() if f.endswith(".pdf")]
+# AREAS = [search(r"(?:[0-9]+?)?\s?([a-zA-Z]+?)?", f)[1] for f in listdir() if f.endswith(".pdf")]
