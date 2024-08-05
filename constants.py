1542fcdbeaf3d916245fcb53dd829bdb97100cdf
Jason Whitman <thejasonplayz@gmail.com>
2024-08-05 14:04:34 -0600

    Remove uneeded constants

--- constants.py
+++ constants.py
@@ -9,12 +9,7 @@ DOCX_TEMPLATE = "317301 KRUPA.docx"
 PDF_TEMPLATE = f"{OUTPUT}/317301 KRUPA.pdf"
 XLSX = f"data.xlsx"
 TEMPLATE = f"{OUTPUT}/template.html"
-REPORT = f"{OUTPUT}/report.pdf"
 METRICS = f"{OUTPUT}/metrics.txt"
-TEMPLATE = f"{OUTPUT}/template.html"
-# TEMPLATE_SPLIT = f"{OUTPUT}/template_split.html"
-# PDF_SPLIT = PDF.split(".")[0]
-# TEMPLATE_SPLIT_PAGES = [f"{OUTPUT}/{f}" for f in listdir(OUTPUT) if search(fr"317301 KRUPA[0-9]+.page", f)]
 PARSER = "html5lib"
 
 class PageNums(Enum):
