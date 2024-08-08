4f54dab43c0d08e0e772e8d3162c484e331ed158
Jason Whitman <thejasonplayz@gmail.com>
2024-08-08 12:37:51 -0600

    Add needed import, Remove uneeded constants, change and add necessary constants

--- constants.py
+++ constants.py
@@ -1,17 +1,18 @@
 from os import getcwd, listdir
+from os.path import isfile
 from re import search
 
 CWD = getcwd()
 CWD_LINUX = CWD.replace("C:", "/mnt/c").replace("\\", "/")
 OUTPUT = "output"
-DOCX_TEMPLATE = "317301 KRUPA.docx"
-PDF_TEMPLATE = f"{OUTPUT}/317301 KRUPA.pdf"
+DOCX_FILES = [f for f in listdir(CWD) if f.endswith(".docx")]
+NEEDED_PDFS = [f"{OUTPUT}/{f.split(".")[0] + ".pdf"}" for f in DOCX_FILES]
+PDF_TEMPLATE = f"{OUTPUT}/template.pdf"
 XLSX = f"data.xlsx"
 TEMPLATE = f"{OUTPUT}/template.html"
 METRICS = f"{OUTPUT}/metrics.txt"
 PARSER = "html5lib"
-DOCX_FILES = [f for f in listdir() if f.endswith(".docx")]
-ID_TO_AREA = dict([search(r"([0-9|a-zA-Z]+)?\s?([0-9|a-zA-Z]+)?", f).groups(2) for f in listdir(f"{OUTPUT}/") if f.endswith(".docx") or f.endswith(".pdf")])
+ID_TO_AREA = dict([search(r"([0-9|a-zA-Z]+)?\s?([0-9|a-zA-Z]+)?", f).groups(2) for f in listdir(CWD) if f.endswith(".docx")])
 XLSX_SERIAL = "Serial #"
 XLSX_DATE = "Date"
 XLSX_REPORT = "Report #"
